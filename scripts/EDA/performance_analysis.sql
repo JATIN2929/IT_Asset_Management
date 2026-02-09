093d45242f8332e82d333f6070cd823dbd359bd5
JATIN2929 <53970836+JATIN2929@users.noreply.github.com>
2026-02-09 21:51:38 +0000

    changes

--- scripts/EDA/performance_analysis.sql
+++ scripts/EDA/performance_analysis.sql
@@ -14,4 +14,19 @@ SQL Functions Used:
 ===============================================================================
 */
 
-SELECT COUNT(*)  FROM Gold.fact_breakfix_table
+with total_issues AS (
+    SELECT 
+        am.[date] AS issue_date,
+        al.product_name,
+        COUNT(new_laptop_model)AS total_breakfix_issues,
+        COUNT(temporary_accessories) AS total_temporary_issues,
+        COUNT(handover_accessories) AS total_handover_issues,
+        COUNT(missing_asset) AS total_missing_issues,
+        COUNT(damage_detail) AS total_damage_issues
+    FROM Gold.fact_daily_asset_management_table am
+    LEFT JOIN Gold.dim_asset_allocation_table al
+        ON al.staff_id = am.staff_id
+    GROUP BY al.product_name
+)
+
+SELECT * FROM total_issues
